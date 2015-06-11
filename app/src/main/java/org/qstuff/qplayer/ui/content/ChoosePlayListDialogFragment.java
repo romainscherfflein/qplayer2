221404f6f84d6df5c6577ba4504225288d982791
qstuff <github@antamauna.net>
2015-06-11 15:47:53 +0200

    show playlists in dialog. add selected track to playlist onclick

--- app/src/main/java/org/qstuff/qplayer/ui/content/ChoosePlayListDialogFragment.java
+++ app/src/main/java/org/qstuff/qplayer/ui/content/ChoosePlayListDialogFragment.java
@@ -1,16 +1,10 @@
 package org.qstuff.qplayer.ui.content;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.Notification;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.ActionMode;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -19,8 +13,14 @@ import android.widget.TextView;
 import com.squareup.otto.Bus;
 
 import org.qstuff.qplayer.R;
+import org.qstuff.qplayer.controller.PlayListController;
+import org.qstuff.qplayer.data.PlayList;
+import org.qstuff.qplayer.data.Track;
+import org.qstuff.qplayer.events.AddTrackToPlayListEvent;
 import org.qstuff.qplayer.events.NewPlayListEvent;
 import org.qstuff.qplayer.ui.AbstractBaseDialogFragment;
+
+import java.util.ArrayList;
 
 import javax.inject.Inject;
 
@@ -28,6 +28,8 @@ import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
 import butterknife.OnEditorAction;
+import butterknife.OnItemClick;
+import butterknife.OnItemLongClick;
 import timber.log.Timber;
 
 /**
@@ -36,25 +38,52 @@ import timber.log.Timber;
 public class ChoosePlayListDialogFragment extends AbstractBaseDialogFragment {
 
     @Inject Bus bus;
-
-    @InjectView(R.id.edittext_new_playlist_name) EditText editText;
+    @Inject PlayListController playListController;
     
-    private PlayListIndexerArrayAdapter playListAdapter;
+    @InjectView(R.id.choose_dialog_fragment_edittext) EditText editText;
+    @InjectView(R.id.choose_dialog_fragment_listview) ListView listView;
+    
+    private PlayListIndexerArrayAdapter<String> playListAdapter;
+    private ArrayList<PlayList>                 playLists;
+    private boolean                             isPlayListList;
+    private Track                               selectedTrack;
 
+    
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View v = inflater.inflate(R.layout.choose_playlist_dialog_fragment, container, false);
         
         ButterKnife.inject(this, v);
         getDialog().setTitle(getString(R.string.choose_playlist_dialog_title));
+        
+        playLists = playListController.getPlayLists();
+        isPlayListList = true;
+        
+        playListAdapter = new PlayListIndexerArrayAdapter<>(
+            getActivity(),
+            R.layout.playlist_item,
+            R.id.playlist_item_text,
+            playListController.getPlayListNames(),
+            isPlayListList);
+        
+        listView.setAdapter(playListAdapter);
+        
         return v;
     }
         
     //
     // Input Handlers
     //
+    @OnItemClick(R.id.choose_dialog_fragment_listview)
+    public void onListItemClicked(int position) {
+        Timber.d("onListItemClicked: pos: " + position);
+
+        PlayList pl = playLists.get(position);
+        bus.post(new AddTrackToPlayListEvent(pl));
+        getDialog().dismiss();
+    }
     
-    @OnEditorAction(R.id.edittext_new_playlist_name)
+    @OnEditorAction(R.id.choose_dialog_fragment_edittext)
     public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
 
         if (event != null && event.getAction() != KeyEvent.ACTION_DOWN) {
